7c20dba41aa468e2a211fbd3d88bd08dacc675e4
mo7hamd2003 <mohmdsteitieh123@gmail.com>
2026-07-16 13:02:36 +0300

    another fix on tailwindcss config file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import * as tailwindcssAnimate from "tailwindcss-animate"
 
 const config: Config = {
   content: ["./src/**/*.{ts,tsx}"],
@@ -10,7 +11,7 @@ const config: Config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: ["tailwindcssAnimate"],
 };
 
 export default config;
